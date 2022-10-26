a983d0bb73301f3ec584d73cea184d1d2b1f6424
mattialicausi <licausivitomattia@gmail.com>
2022-10-26 13:41:17 +0200

    inserito titolo in dom tramite vue.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//creo costante prensa da Vue
+const {createApp} = Vue;
+
+
+//uso funzione di Vue per creare oggetto
+const titleHTML = createApp({
+    data(){
+        return {
+            title: 'Vue Hello'
+        }
+    }
+});
+titleHTML.mount('#app');
+
